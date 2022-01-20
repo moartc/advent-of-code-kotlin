14bcff486f891fe884240276cf3118ebedc640df
mo <allenszwester@gmail.com>
2022-01-20 23:31:21 +0100

    Day 6.

---
+++ src/main/kotlin/solutions/day06/Solution.kt
@@ -0,0 +1,67 @@
+package solutions.day06
+
+import utils.Resources
+
+fun main() {
+
+    val inputLines = Resources.getLines(6)
+    println("part1 = " + part1(inputLines))
+    println("part2 = " + part2(inputLines))
+}
+
+fun part1(input: List<String>): Int {
+    return input.fold(Array(1000) { BooleanArray(1000) }) { acc, command ->
+        acc.applyCommand(
+            getCommandFromString(
+                command
+            )
+        )
+    }.flatMap(BooleanArray::asIterable).count()
+}
+
+fun part2(input: List<String>): Int {
+    return input.fold(Array(1000) { IntArray(1000) }) { acc, command ->
+        acc.applyCommand(
+            getCommandFromString(
+                command
+            )
+        )
+    }.flatMap(IntArray::asIterable).sum()
+}
+
+fun getCommandFromString(command: String): Command {
+    val regex = Regex("(.+) (\\d+),(\\d+) through (\\d+),(\\d+)")
+    val groups = regex.matchEntire(command)!!.groups
+    return Command(
+        groups[1]!!.value, Pair(groups[2]!!.value.toInt(), groups[3]!!.value.toInt()),
+        Pair(groups[4]!!.value.toInt(), groups[5]!!.value.toInt())
+    )
+}
+
+fun Array<BooleanArray>.applyCommand(command: Command): Array<BooleanArray> {
+    (command.start.first..command.end.first)
+        .flatMap { a -> (command.start.second..command.end.second).map { b -> a to b } }
+        .forEach { pair ->
+            when (command.type) {
+                "toggle" -> this[pair.first][pair.second] = !this[pair.first][pair.second]
+                "turn on" -> this[pair.first][pair.second] = true
+                "turn off" -> this[pair.first][pair.second] = false
+            }
+        }
+    return this
+}
+
+fun Array<IntArray>.applyCommand(command: Command): Array<IntArray> {
+    (command.start.first..command.end.first)
+        .flatMap { a -> (command.start.second..command.end.second).map { b -> a to b } }
+        .forEach { pair ->
+            when (command.type) {
+                "toggle" -> this[pair.first][pair.second] += 2
+                "turn on" -> this[pair.first][pair.second] += 1
+                "turn off" -> if (this[pair.first][pair.second] > 0) this[pair.first][pair.second]-- else this[pair.first][pair.second] = 0
+            }
+        }
+    return this
+}
+
+data class Command(val type: String, val start: Pair<Int, Int>, val end: Pair<Int, Int>)
---
+++ src/test/kotlin/solutions/day06/SolutionKtTest.kt
@@ -0,0 +1,70 @@
+package solutions.day06
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class SolutionKtTest {
+
+    @Test
+    fun part1Test1() {
+        val input = listOf("turn on 0,0 through 999,999")
+        assertEquals(1000 * 1000, part1(input))
+    }
+
+    @Test
+    fun part1Test2() {
+        val input = listOf("turn on 0,0 through 999,999", "toggle 0,0 through 999,0")
+        assertEquals((1000 * 1000) - 1000, part1(input))
+    }
+
+    @Test
+    fun part1Test3() {
+        val input =
+            listOf("turn on 0,0 through 999,999", "toggle 0,0 through 999,0", "turn off 499,499 through 500,500")
+        assertEquals((1000 * 1000) - 1000 - 4, part1(input))
+    }
+
+    @Test
+    fun part2Test1() {
+        val input = listOf("turn on 0,0 through 0,0")
+        assertEquals(1, part2(input))
+    }
+
+    @Test
+    fun part2Test2() {
+        val input = listOf("toggle 0,0 through 999,999")
+        assertEquals(2000000, part2(input))
+    }
+
+    @Test
+    fun part2Test3() {
+        val input = listOf("turn on 0,0 through 0,0", "turn on 0,0 through 0,0", "turn on 0,0 through 0,0")
+        assertEquals(3, part2(input))
+    }
+
+    @Test
+    fun part2Test4() {
+        val input = listOf("toggle 0,0 through 999,999", "turn on 0,0 through 0,0")
+        assertEquals(2000000+1, part2(input))
+    }
+
+    @Test
+    fun part2Test5() {
+        val input = listOf("toggle 0,0 through 999,999", "turn off 0,0 through 0,0")
+        assertEquals(2000000-1, part2(input))
+    }
+
+    @Test
+    fun part2Test6() {
+        val input = listOf("turn off 0,0 through 0,0", "turn off 0,0 through 0,0", "toggle 0,0 through 999,999")
+        assertEquals(2000000, part2(input))
+    }
+
+    @Test
+    fun getCommandFromString() {
+        val input = "turn on 12,55 through 999,992"
+        val expected = Command("turn on", Pair(12, 55), Pair(999, 992))
+        val actual = getCommandFromString(input)
+        assertEquals(expected, actual)
+    }
+}
