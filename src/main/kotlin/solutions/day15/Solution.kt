4662d49b396f5af1a8600f4bd4852dab5b188192
mo <allenszwester@gmail.com>
2022-02-04 23:31:12 +0100

    Day 15.

--- src/main/kotlin/solutions/day15/Solution.kt
+++ src/main/kotlin/solutions/day15/Solution.kt
@@ -1,51 +1,40 @@
 package solutions.day15
 
 import utils.Resources
+import java.util.function.IntPredicate
 
 fun main() {
 
     val inputLines = Resources.getLines(15)
     val ingredients = parseReindeerList(inputLines)
-
-    val part1 = part1(ingredients)
-    val part2 = part2(ingredients)
-    println("part1 = $part1 OK? - ${part1 == 13882464}")
-    println("part2 = $part2 OK? - ${part2 == 11171160}")
+    println("part1 = " + part1(ingredients))
+    println("part2 = " + part2(ingredients))
 }
 
-fun part1(ingredients: List<Ingredient>): Int {
+fun part1(ingredients: List<Ingredient>) = getAnswer(ingredients) { true }
 
-    return generatePermutations(100, ingredients.size).maxOf { perm ->
-        val capacity = ingredients.mapIndexed { index, ingredient -> ingredient.capacity * perm[index] }.sum()
-        val durability = ingredients.mapIndexed { index, ingredient -> ingredient.durability * perm[index] }.sum()
-        val flavor = ingredients.mapIndexed { index, ingredient -> ingredient.flavor * perm[index] }.sum()
-        val texture = ingredients.mapIndexed { index, ingredient -> ingredient.texture * perm[index] }.sum()
-        (if (capacity > 0) capacity else 0) * (if (durability > 0) durability else 0) * (if (flavor > 0) flavor else 0) * (if (texture > 0) texture else 0)
-    }
-}
+fun part2(ingredients: List<Ingredient>) = getAnswer(ingredients) { it == 500 }
 
-fun part2(ingredients: List<Ingredient>): Int {
-    return generatePermutations(100, ingredients.size).map { perm ->
-        val capacity = ingredients.mapIndexed { index, ingredient -> ingredient.capacity * perm[index] }.sum()
-        val durability = ingredients.mapIndexed { index, ingredient -> ingredient.durability * perm[index] }.sum()
-        val flavor = ingredients.mapIndexed { index, ingredient -> ingredient.flavor * perm[index] }.sum()
-        val texture = ingredients.mapIndexed { index, ingredient -> ingredient.texture * perm[index] }.sum()
-        val total =
-            (if (capacity > 0) capacity else 0) * (if (durability > 0) durability else 0) * (if (flavor > 0) flavor else 0) * (if (texture > 0) texture else 0)
-        val calories = ingredients.mapIndexed { index, ingredient -> ingredient.calories * perm[index] }.sum()
-        Pair(total, calories)
-    }.filter { it.second == 500 }.maxByOrNull { it.first }!!.first
-}
+fun getAnswer(ingredients: List<Ingredient>, kcalFilter: IntPredicate) =
+    generatePermutations(100, ingredients.size)
+        .asSequence()
+        .map { permutation -> permutation.mapIndexed { permutationIndex, value -> ingredients[permutationIndex].properties.map { p -> p * value } } }
+        .map(::transposeArray)
+        .map { property -> property.map(List<Int>::sum).map { i -> if (i > 0) i else 0 } }
+        .filter { result -> kcalFilter.test(result[4]) }
+        .maxOf { it.take(4).reduce(Int::times) }
+
+fun transposeArray(list: List<List<Int>>) = List(list.first().size) { i -> list.map { it[i] } }
 
 fun generatePermutations(totalSum: Int, size: Int) =
-    (0..totalSum).map { value -> appendValueToRow2(totalSum - value, size, listOf(value)) }.flatten()
+    (0..totalSum).map { value -> appendValueToRow(totalSum - value, size, listOf(value)) }.flatten()
 
-fun appendValueToRow2(sum: Int, totalSize: Int, currentRow: List<Int>): List<List<Int>> =
+fun appendValueToRow(sum: Int, totalSize: Int, currentRow: List<Int>): List<List<Int>> =
     currentRow.map {
         return if (currentRow.size == totalSize - 1) {
             listOf(currentRow + sum)
         } else {
-            (0..sum).map { value -> appendValueToRow2(sum - value, totalSize, currentRow + value) }.flatten()
+            (0..sum).map { value -> appendValueToRow(sum - value, totalSize, currentRow + value) }.flatten()
         }
     }
 
@@ -53,12 +42,8 @@ fun parseReindeerList(input: List<String>) =
     input.map { line -> """.* (-?\d+).* (-?\d+).* (-?\d+).* (-?\d+).* (-?\d+)""".toRegex().matchEntire(line) }
         .map { matchResult -> matchResult!!.groups }.map { group -> Ingredient(group) }
 
-class Ingredient(val capacity: Int, val durability: Int, val flavor: Int, val texture: Int, val calories: Int) {
+class Ingredient(val properties: List<Int>) {
     constructor(mgc: MatchGroupCollection) : this(
-        mgc[1]!!.value.toInt(),
-        mgc[2]!!.value.toInt(),
-        mgc[3]!!.value.toInt(),
-        mgc[4]!!.value.toInt(),
-        mgc[5]!!.value.toInt()
+        mgc.asSequence().drop(1).take(5).map { group -> group!!.value.toInt() }.toList()
     )
 }
--- src/test/kotlin/solutions/day15/SolutionKtTest.kt
+++ src/test/kotlin/solutions/day15/SolutionKtTest.kt
@@ -10,7 +10,7 @@ internal class SolutionKtTest {
     fun part1() {
         val input = listOf(
             "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8",
-            "Cinnamon: capac ity 2, durability 3, flavor -2, texture -1, calories 3"
+            "Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3"
         )
         assertEquals(62842880, part1(parseReindeerList(input)))
     }
@@ -19,7 +19,7 @@ internal class SolutionKtTest {
     fun part2() {
         val input = listOf(
             "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8",
-            "Cinnamon: capac ity 2, durability 3, flavor -2, texture -1, calories 3"
+            "Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3"
         )
         assertEquals(57600000, part2(parseReindeerList(input)))
     }
