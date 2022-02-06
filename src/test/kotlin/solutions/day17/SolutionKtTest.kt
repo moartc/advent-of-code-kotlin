6fd56f2b54bf2b33735adba8429df16325fc5d5e
mo <allenszwester@gmail.com>
2022-02-06 22:20:39 +0100

    Day 17.

---
+++ src/test/kotlin/solutions/day17/SolutionKtTest.kt
@@ -0,0 +1,20 @@
+package solutions.day17
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class SolutionKtTest {
+
+    @Test
+    fun part1Test() {
+        val input = listOf(20, 15, 10, 5, 5)
+        assertEquals(4, part1(input, 25))
+    }
+
+    @Test
+    fun part2Test() {
+        val input = listOf(20, 15, 10, 5, 5)
+        assertEquals(3, part2(input, 25))
+    }
+}
