8227125d2b934e7d3f68ce36177dc438f93d8dce
mo <allenszwester@gmail.com>
2022-02-17 22:42:54 +0100

    Day 25

---
+++ src/main/kotlin/solutions/day25/Solution.kt
@@ -0,0 +1,12 @@
+package solutions.day25
+
+import utils.Resources
+
+fun main() {
+    val input = getRowAndColumn(Resources.getLine(25))
+    println("final part = " + getCode(input[0].toInt(), input[1].toInt()))
+}
+
+fun getCode(row: Int, col: Int) = generateSequence(20151125L) { (it * 252533L) % 33554393L }.take(getNumber(row, col)).last()
+fun getNumber(row: Int, col: Int) = ((1 until row + col).sum() - row + 1)
+fun getRowAndColumn(input: String) = Regex(""".*row (\d+), column (\d+)""").find(input)!!.groupValues.takeLast(2)
