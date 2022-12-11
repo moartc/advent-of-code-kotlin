6cd3be3aec26d089bd8af4b437182eb3b83542f3
mo <allenszwester@gmail.com>
2022-12-11 22:17:10 +0100

    Some utils

--- src/main/kotlin/utils/Collections.kt
+++ src/main/kotlin/utils/Collections.kt
@@ -6,5 +6,10 @@ fun <T> List<List<T>>.transpose(): List<List<T>> {
     return result
 }
 
-fun <T> List<List<T>>.deepCopyMutable() : MutableList<MutableList<T>> = this.map { it.toMutableList() }.toMutableList()
-fun <T> List<List<T>>.deepCopyImmutable() : List<List<T>> = this.map { it.toList() }.toList()
+fun <T> List<List<T>>.deepCopyMutable(): MutableList<MutableList<T>> = this.map { it.toMutableList() }.toMutableList()
+fun <T> List<List<T>>.deepCopyImmutable(): List<List<T>> = this.map { it.toList() }.toList()
+
+fun List<Int>.product() = this.reduce { acc, number -> acc * number }
+fun List<Long>.product() = this.reduce { acc, number -> acc * number }
+
+
--- src/main/kotlin/utils/Parser.kt
+++ src/main/kotlin/utils/Parser.kt
@@ -1,6 +1,27 @@
 package utils
 
-fun String.getInts(): List<Int> = """\d+""".toRegex().findAll(this).map(MatchResult::value).map(String::toInt).toList()
-fun String.getInt(): Int? = """\d+""".toRegex().find(this)?.value?.toInt()
+fun String.getUInts(): List<Int> = """\d+""".toRegex().findAll(this).map(MatchResult::value).map(String::toInt).toList()
+fun String.getUInt(): Int? = """\d+""".toRegex().find(this)?.value?.toInt()
+
+fun String.getInts(): List<Int> = """-?\d+""".toRegex().findAll(this).map(MatchResult::value).map(String::toInt).toList()
+fun String.getInt(): Int? = """-?\d+""".toRegex().find(this)?.value?.toInt()
 fun String.getChars(start: Int, interval: Int): List<Char?> = this.filterIndexed { index, _ -> index == start || (index - start) % interval == 0 }
     .map { if(it.isLetter()) it else null }.toList()
+
+
+inline fun <T> Iterable<T>.splitOn(predicate: (T) -> Boolean): List<List<T>> {
+    val toRet = mutableListOf<MutableList<T>>()
+    var newList = mutableListOf<T>()
+    this.forEach {
+        if(predicate(it)) {
+            toRet += newList
+            newList = mutableListOf()
+        } else {
+            newList += it
+        }
+    }
+    toRet += newList
+    return toRet
+}
+
+fun Iterable<String>.splitOnEmpty(): List<List<String>> = this.splitOn { it.isEmpty() }
