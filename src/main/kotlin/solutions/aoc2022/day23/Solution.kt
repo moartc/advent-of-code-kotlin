7435d195dc97bd2424591fb35df4cc06b443865f
szwester <allenszwester@gmail.com>
2022-12-23 08:38:30 +0100

    Day 2022/23 - cleanup

--- src/main/kotlin/solutions/aoc2022/day23/Solution.kt
+++ src/main/kotlin/solutions/aoc2022/day23/Solution.kt
@@ -9,94 +9,66 @@ fun main() {
     println("part2 = ${part2(input)}")
 }
 
+fun part1(input: List<String>): Int {
+
+    val dirs = mutableListOf("N", "S", "W", "E")
+    val points = input.flatMapIndexed { y, line -> line.mapIndexed { x, c -> if (c == '#') Elf(y, x) else null } }.filterNotNull()
+    var afterRound = points.toList()
+
+    repeat(10) {
+        val propose = afterRound.map { getNewPos(afterRound, it, dirs) }
+        afterRound = propose.mapIndexed { idx, p -> if (propose.count { p2 -> p2 == p } == 1) p else afterRound[idx] }
+        val firstDir = dirs.first()
+        dirs.removeAt(0)
+        dirs.add(firstDir)
+    }
+    val horizontalSize = afterRound.maxOf { e -> e.x } + 1 - afterRound.minOf { e -> e.x }
+    val verticalSize = afterRound.maxOf { e -> e.y } + 1 - afterRound.minOf { e -> e.y }
+    return horizontalSize * verticalSize - points.size
+}
+
 fun part2(input: List<String>): Int {
 
     val dirs = mutableListOf("N", "S", "W", "E")
     val points = input.flatMapIndexed { y, line -> line.mapIndexed { x, c -> if (c == '#') Elf(y, x) else null } }.filterNotNull()
 
-    var afterRound = points.toMutableList()
-    var prev = mutableListOf<Elf>()
+    var afterRound = points.toList()
+    var prev = listOf<Elf>()
 
     var ctr = 0
     while (true) {
         ctr += 1
         val propose = afterRound.map { getNewPos(afterRound, it, dirs) }
-
-        afterRound = propose.mapIndexed { idx, p ->
-            if (propose.count { p2 -> p2 == p } == 1) {
-                p
-            } else {
-                afterRound[idx]
-            }
-        }.toMutableList()
-
+        afterRound = propose.mapIndexed { idx, p -> if (propose.count { p2 -> p2 == p } == 1) p else afterRound[idx] }
         if (prev == afterRound) {
             return ctr
         }
         prev = afterRound
-
-        val f = dirs.first()
+        val firstDir = dirs.first()
         dirs.removeAt(0)
-        dirs.add(f)
+        dirs.add(firstDir)
     }
 }
 
-
-data class Elf(val y: Int, val x: Int) {}
-
-fun part1(input: List<String>): Int {
-
-    val dirs = mutableListOf("N", "S", "W", "E")
-    val points = input.flatMapIndexed { y, line -> line.mapIndexed { x, c -> if (c == '#') Elf(y, x) else null } }.filterNotNull()
-    var afterRound = points.toMutableList()
-
-    repeat(10) {
-
-        val propose = afterRound.map { getNewPos(afterRound, it, dirs) }
-
-        afterRound = propose.mapIndexed { idx, p ->
-            if (propose.count { p2 -> p2 == p } == 1) {
-                p
-            } else {
-                afterRound[idx]
-            }
-        }.toMutableList()
-
-        val f = dirs.first()
-        dirs.removeAt(0)
-        dirs.add(f)
-    }
-
-    val mostE = afterRound.maxOf { e -> e.x }
-    val mostW = afterRound.minOf { e -> e.x }
-    val mostS = afterRound.maxOf { e -> e.y }
-    val mostN = afterRound.minOf { e -> e.y }
-
-    val horizontal = mostE + 1 - mostW
-    val vertical = mostS + 1 - mostN
-    val numOfPos = horizontal * vertical
-    return numOfPos - points.size
-}
-
+data class Elf(val y: Int, val x: Int)
 
 fun getNewPos(allPos: List<Elf>, elf: Elf, dirs: MutableList<String>): Elf {
 
-    val y = elf.y
-    val x = elf.x
+    val (y, x) = elf
 
     val l1 = listOf(Elf(y - 1, x), Elf(y - 1, x - 1), Elf(y - 1, x + 1))
-    val north = !allPos.any { a -> l1.contains(a) }
+    val north = allPos.none { a -> l1.contains(a) }
 
     val l2 = listOf(Elf(y + 1, x), Elf(y + 1, x - 1), Elf(y + 1, x + 1))
-    val south = !allPos.any { a -> l2.contains(a) }
+    val south = allPos.none { a -> l2.contains(a) }
 
     val l3 = listOf(Elf(y, x - 1), Elf(y + 1, x - 1), Elf(y - 1, x - 1))
-    val west = !allPos.any { a -> l3.contains(a) }
+    val west = allPos.none { a -> l3.contains(a) }
 
     val l4 = listOf(Elf(y, x + 1), Elf(y + 1, x + 1), Elf(y - 1, x + 1))
-    val east = !allPos.any { a -> l4.contains(a) }
+    val east = allPos.none { a -> l4.contains(a) }
 
-    val possible = listOf(
+    val adjacentPositions = listOf(
         Elf(y - 1, x),
         Elf(y - 1, x - 1),
         Elf(y - 1, x + 1),
@@ -107,8 +79,7 @@ fun getNewPos(allPos: List<Elf>, elf: Elf, dirs: MutableList<String>): Elf {
         Elf(y, x + 1)
     )
 
-    val doAnything = allPos.any { a -> possible.contains(a) }
-    if (doAnything) {
+    if (allPos.any { a -> adjacentPositions.contains(a) }) {
         dirs.forEach {
             when (it) {
                 "N" -> if (north) return Elf(y - 1, x)
@@ -117,9 +88,6 @@ fun getNewPos(allPos: List<Elf>, elf: Elf, dirs: MutableList<String>): Elf {
                 "E" -> if (east) return Elf(y, x + 1)
             }
         }
-    } else {
-        return elf
     }
     return elf
-
 }
