6958c5ac884278b3911767089da8f31f5bfa05a3
mo <allenszwester@gmail.com>
2022-12-28 21:25:19 +0100

    Day 2022/21 - refactored

--- src/main/kotlin/solutions/aoc2022/day21/Solution.kt
+++ src/main/kotlin/solutions/aoc2022/day21/Solution.kt
@@ -1,51 +1,19 @@
-package solutions.aoc2022.day21
-
 import utils.Resources
 
 fun main() {
     val input = Resources.getLines(2022, 21)
 
     println("part1 = ${part1(input)}")
-    println("part2 to solve in wolfram alpha = ${part2(input)}")
+    println("part2 = ${part2(input)}")
 }
 
-
-fun part2(input: List<String>): String {
-
-    val registerM = mutableMapOf<String, String>()
-    val fixedInput = input.map { i ->
-        if (i.contains("root")) i.replace("+", "=")
-        else if(i.contains("humn:")) {
-            val split = i.split(" ")
-            split[0] + " x"
-        } else {
-            i
-        }
-    }
-    val ops = parse(fixedInput)
-
-    ops.forEach { i ->
-        when (i) {
-            is Yell -> registerM[i.monkey] = i.value.toString()
-            is Yell2 -> registerM[i.monkey] = i.value
-        }
-    }
-
-    return solve(ops, registerM)
-
-}
-
-
-fun parse(input: List<String>): List<Op> {
-    val list = input.map {
+fun part1(input: List<String>): Long {
+    val ops = input.map {
         val vals = it.split(" ")
         if (vals.size == 2) {
             val name = vals[0].dropLast(1)
             val value = vals[1]
-            if (value == "x")
-                Yell2(name, value)
-            else
-                Yell(name, value.toLong())
+            Yell(name, value.toLong())
         } else {
             val name = vals[0].dropLast(1)
             val fMonk = vals[1]
@@ -54,110 +22,93 @@ fun parse(input: List<String>): List<Op> {
             Complex(name, fMonk, operator[0], sMonk)
         }
     }
-    return list
-
+    return getValue("root", ops)
 }
 
-fun solve(ops: List<Op>, register: MutableMap<String, String>): String {
-    while (!register.containsKey("root")) {
-        ops.forEach {
-            when (it) {
-                is Complex -> {
-                    val fM = it.first
-                    val sM = it.second
-                    if (register.containsKey(fM) && register.containsKey(sM)) {
-                        val op = it.operation
-                        when (op) {
-                            '+' -> {
-                                val ff = register[fM]!!
-                                val fs = register[sM]!!
-                                if (ff.contains("x") || fs.contains("x")) {
-                                    if (ff.equals("0"))
-                                        register[it.monkey] = fs
-                                    else if (fs.equals("0"))
-                                        register[it.monkey] = ff
-                                    else
-                                        register[it.monkey] = "(" + ff + "+" + fs + ")"
+fun getValue(s: String, ops: List<Op>): Long {
+    val m = ops.first { it.monkey == s }
+    if (m is Yell)
+        return m.value
+    else if (m is Complex) {
+        return when (m.operation) {
+            '+' -> getValue(m.first, ops) + getValue(m.second, ops)
+            '-' -> getValue(m.first, ops) - getValue(m.second, ops)
+            '/' -> getValue(m.first, ops) / getValue(m.second, ops)
+            else -> getValue(m.first, ops) * getValue(m.second, ops)
+        }
+    }
+    return -1
+}
 
-                                } else {
-                                    val res = ff.toLong() + fs.toLong()
-                                    register[it.monkey] = res.toString()
-                                }
+fun part2(input: List<String>): Any {
 
-                            }
+    val ops = input.map {
+        val vals = it.split(" ")
+        if (vals.size == 2) {
+            val name = vals[0].dropLast(1)
+            val value = vals[1]
+            if (name != "humn") {
+                Yell(name, value.toLong())
+            } else {
+                YellX(name)
+            }
+        } else {
+            val name = vals[0].dropLast(1)
+            val fMonk = vals[1]
+            val sMonk = vals[3]
+            val operator = vals[2]
+            Complex(name, fMonk, operator[0], sMonk)
+        }
+    }
+    val root = ops.first { it.monkey == "root" } as Complex
+    val rightSideValue = getValue(root.second, ops)
+    return getHumnValue(root.first, ops, rightSideValue)
+}
 
-                            '-' -> {
-                                val ff = register[fM]!!
-                                val fs = register[sM]!!
-                                if (ff.contains("x") || fs.contains("x")) {
-                                    if (ff.equals("0"))
-                                        register[it.monkey] = fs
-                                    else if (fs.equals("0"))
-                                        register[it.monkey] = ff
-                                    else
-                                        register[it.monkey] = "(" + ff + "-" + fs + ")"
-                                } else {
-                                    val res = ff.toLong() - fs.toLong()
-                                    register[it.monkey] = res.toString()
-                                }
-                            }
-
-                            '/' -> {
-                                val ff = register[fM]!!
-                                val fs = register[sM]!!
-                                if (ff.contains("x") || fs.contains("x")) {
-                                    if (ff == ("0"))
-                                        register[it.monkey] = "0"
-                                    else
-                                        register[it.monkey] = "(" + ff + "/" + fs + ")"
-                                } else {
-                                    val res = ff.toLong() / fs.toLong()
-                                    register[it.monkey] = res.toString()
-                                }
-                            }
-
-                            '*' -> {
-                                val ff = register[fM]!!
-                                val fs = register[sM]!!
-                                if (ff.contains("x") || fs.contains("x")) {
-                                    if (ff == ("0") || fs == "0")
-                                        register[it.monkey] = "0"
-                                    else
-                                        register[it.monkey] = "(" + ff + "*" + fs + ")"
-                                } else {
-                                    val res = ff.toLong() * fs.toLong()
-                                    register[it.monkey] = res.toString()
-                                }
-                            }
-
-                            '=' -> {
-                                val ff = register[fM]!!
-                                val fs = register[sM]!!
-                                register[it.monkey] = "(" + ff + "=" + fs + ")"
-
-                            }
-                        }
-                    }
+fun getHumnValue(reg: String, ops: List<Op>, rightSideValue: Long): Long {
+    if (reg == "humn") {
+        return rightSideValue
+    } else {
+        val r = ops.first { it.monkey == reg }
+        if (r is Yell) {
+            return r.value
+        } else if (r is Complex) {
+            val f = r.first
+            val s = r.second
+            if (isHumn(s, ops)) {
+                return when (r.operation) {
+                    '/' -> getHumnValue(s, ops, getValue(f, ops) / rightSideValue)  // 12/x = 3
+                    '+' -> getHumnValue(s, ops, rightSideValue - getValue(f, ops))  // 12+x = 3
+                    '-' -> getHumnValue(s, ops, getValue(f, ops) - rightSideValue)  // 12-x = 3
+                    else -> getHumnValue(s, ops, rightSideValue / getValue(f, ops)) // 12*x = 3
+                }
+            } else if (isHumn(f, ops)) {
+                return when (r.operation) {
+                    '/' -> getHumnValue(f, ops, rightSideValue * getValue(s, ops))
+                    '+' -> getHumnValue(f, ops, rightSideValue - getValue(s, ops))
+                    '-' -> getHumnValue(f, ops, rightSideValue + getValue(s, ops))  // x-12 = 3
+                    else -> getHumnValue(f, ops, rightSideValue / getValue(s, ops))
                 }
             }
         }
     }
-    val res = register["root"]!!
-    return res
+    return -12
 }
-fun part1(input: List<String>): String {
 
-    val register = mutableMapOf<String, String>()
-    val ops = parse(input)
-    ops.forEach { i ->
-        when (i) {
-            is Yell -> register[i.monkey] = i.value.toString()
+fun isHumn(s: String, ops: List<Op>): Boolean {
+    return if (s == "humn") {
+        true
+    } else {
+        val op = ops.first { it.monkey == s }
+        if (op is Yell) {
+            false
+        } else {
+            isHumn((op as Complex).first, ops) || isHumn(op.second, ops)
         }
     }
-    return solve(ops, register)
 }
 
 abstract class Op(val monkey: String)
-class Yell2(monkey: String, val value: String) : Op(monkey)
 class Yell(monkey: String, val value: Long) : Op(monkey)
+class YellX(monkey: String) : Op(monkey)
 class Complex(monkey: String, val first: String, val operation: Char, val second: String) : Op(monkey)
