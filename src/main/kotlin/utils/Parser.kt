ef69089ea62b398f4a53e992f85392f11ad16e96
szwester <allenszwester@gmail.com>
2023-12-05 22:01:04 +0100

    day 5 2023 - refactoring + utils

--- src/main/kotlin/utils/Parser.kt
+++ src/main/kotlin/utils/Parser.kt
@@ -5,8 +5,11 @@ fun String.getUInt(): Int? = """\d+""".toRegex().find(this)?.value?.toInt()
 
 fun String.getInts(): List<Int> = """-?\d+""".toRegex().findAll(this).map(MatchResult::value).map(String::toInt).toList()
 fun String.getInt(): Int? = """-?\d+""".toRegex().find(this)?.value?.toInt()
-fun String.getChars(start: Int, interval: Int): List<Char?> = this.filterIndexed { index, _ -> index == start || (index - start) % interval == 0 }
-    .map { if (it.isLetter()) it else null }.toList()
+
+fun String.getLongs(): List<Long> = """-?\d+""".toRegex().findAll(this).map(MatchResult::value).map(String::toLong).toList()
+fun String.getChars(start: Int, interval: Int): List<Char?> =
+    this.filterIndexed { index, _ -> index == start || (index - start) % interval == 0 }
+        .map { if (it.isLetter()) it else null }.toList()
 
 fun parseArray(line: String): List<Any> {
 
