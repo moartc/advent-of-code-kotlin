c2aabd63876995bebf398595e96fab0bde9810e6
szwester <allenszwester@gmail.com>
2023-12-07 21:52:47 +0100

    day 7 2023 - refactoring

--- src/main/kotlin/solutions/aoc2023/day07/Solution.kt
+++ src/main/kotlin/solutions/aoc2023/day07/Solution.kt
@@ -1,229 +1,78 @@
 package solutions.aoc2023.day07
 
 import utils.Resources
-import utils.splitOn
-import kotlin.math.log
+import kotlin.reflect.KFunction1
 
 fun main() {
 
-    val inputLine =
-        Resources.getLines(2023, 7)
-//        Resources.getLinesExample(2023, 7)
-    println("part1 = ${part1(inputLine)}")
-    println("part2 = ${part2(inputLine)}")
+    val inputLine = Resources.getLines(2023, 7)
+    println("part1 = ${part1(inputLine) == 252052080}")
+    println("part2 = ${part2(inputLine) == 252898370}")
 }
-
-fun part2(input: List<String>): Int {
-    val vals = listOf<Char>('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J')
-    fun getValHand(hand: Map<Char, Int>): Int {
-
-        val get = hand.get('J')
-
-        val filter = hand.filter { k -> k.key != 'J' }.toMutableMap()
-
-
-        if (filter.size == 0) {
-            return 7
-        }
-        val sortedBy = filter.entries.sortedByDescending { it.value }.toMutableList()
-        val value = sortedBy.first().value
-
-        if (get != null) {
-            sortedBy[0].setValue(value + get)
-        }
-
-
-        if (sortedBy[0].value == 5) {
-            return 7
-        }
-
-        if (sortedBy[0].value == 4) {
-            return 6
-        }
-
-        if (sortedBy[0].value == 3) { // full
-            if (sortedBy[1].value == 2) {
-                return 5
-
-            } else {  // just 3
-                return 4
-            }
-        }
-
-        if (sortedBy[0].value == 2) {
-            if (sortedBy[1].value == 2) { //2 pairs
-                return 3
-            } else { // pair
-                return 2
-            }
-        }
-
-
-        return 1 // single
-
-    }
-
-    val mComparator = Comparator<Pair<Pair<Map<Char, Int>, String>, Int>> { o1, o2 ->
-        val f = getValHand(o1.first.first)
-        val s = getValHand(o2.first.first)
-
-
-        "compare ${o1.first.second} with ${o2.first.second}".log()
-        when {
-            f > s -> 1
-            f < s -> -1
-            else -> {
-                var res = 0
-                for (i in 0..4) {
-
-                    o1.first.second[i].log("c1")
-                    o2.first.second[i].log("c2")
-                    val id1 = vals.indexOf(o1.first.second[i])
-                    val id2 = vals.indexOf(o2.first.second[i])
-                    if (id1 < id2) {
-                        res = 1
-                        break
-                    }
-                    if (id2 < id1) {
-                        res = -1
-                        break
-                    }
-                }
-                "equals?".log()
-                res
-            }
-        }
-    }
-
-    val maps = input.map { line ->
-
-        val split = line.split(" ")
-        val hand = split[0].groupingBy { it }.eachCount()
-        val bid = split[1].toInt()
-        (hand to split[0]) to bid
-    }
-
-    val sortedWith = maps.sortedWith(mComparator)
-
-    sortedWith.log("sorted")
-
-    var res = 0
-    var ctr = 1
-    sortedWith.forEach {
-        val i = it.second * ctr
-        ctr++
-        it.first.log("hand check")
-        res += i
-    }
-
-
-    res.log("result ")
-    return res
-}
-
 
 fun part1(input: List<String>): Int {
 
-    val vals = listOf<Char>('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
-
     fun getValHand(hand: Map<Char, Int>): Int {
-
-
         val sortedBy = hand.entries.sortedByDescending { it.value }.toMutableList()
-
-
-//251014372 not right
-
-
-        if (sortedBy[0].value == 5) {
-            return 7
-        }
-
-        if (sortedBy[0].value == 4) {
-            return 6
-        }
-
-        if (sortedBy[0].value == 3) { // full
-            if (sortedBy[1].value == 2) {
-                return 5
-
-            } else {  // just 3
-                return 4
-            }
-        }
-
-        if (sortedBy[0].value == 2) {
-            if (sortedBy[1].value == 2) { //2 pairs
-                return 3
-            } else { // pair
-                return 2
-            }
-        }
-        return 1 // single
-
-    }
-
-
-    input.forEach { it.log("p1") }
-
-
-    val mComparator = Comparator<Pair<Pair<Map<Char, Int>, String>, Int>> { o1, o2 ->
-        val f = getValHand(o1.first.first)
-        val s = getValHand(o2.first.first)
-
-
-        "compare ${o1.first.second} with ${o2.first.second}".log()
-        when {
-            f > s -> 1
-            f < s -> -1
-            else -> {
-                var res = 0
-                for (i in 0..4) {
-
-                    o1.first.second[i].log("c1")
-                    o2.first.second[i].log("c2")
-                    val id1 = vals.indexOf(o1.first.second[i])
-                    val id2 = vals.indexOf(o2.first.second[i])
-                    if (id1 < id2) {
-                        res = 1
-                        break
-                    }
-                    if (id2 < id1) {
-                        res = -1
-                        break
-                    }
-                }
-                "equals?".log()
-                res
-            }
+        return when (sortedBy[0].value) {
+            5 -> 7                                      // five of a kind
+            4 -> 6                                      // 5 of a kind
+            3 -> if (sortedBy[1].value == 2) 5 else 4   // full house else three of a kind
+            2 -> if (sortedBy[1].value == 2) 3 else 2   // two pair else pair
+            else -> 1                                   // high card
         }
     }
 
-    val maps = input.map { line ->
-
-        val split = line.split(" ")
-        val hand = split[0].groupingBy { it }.eachCount()
-        val bid = split[1].toInt()
-        (hand to split[0]) to bid
-    }
-
-    val sortedWith = maps.sortedWith(mComparator)
-
-    var res = 0
-    var ctr = 1
-    sortedWith.forEach {
-        val i = it.second * ctr
-        ctr++
-        it.first.log("hand check")
-        res += i
-    }
-    res.log("result ")
-    return res
+    val labels = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2').reversed()
+    return solve(input, ::getValHand, labels)
 }
 
 
-private fun <T> T.log(): T = also { println("%s".format(this)) }
-private fun <T> T.log(comment: String): T = also { println("%s: %s".format(comment, this)) }
+fun part2(input: List<String>): Int {
 
+    fun getValHand(hand: Map<Char, Int>): Int {
+        val jInHand = hand.getOrDefault('J', 0)
+        val filter = hand.filter { k -> k.key != 'J' }.toMutableMap()
+        if (filter.isEmpty()) { // there are only 'J'
+            return 7
+        }
+        // sort values in hand descending
+        val sortedBy = filter.entries.sortedByDescending { it.value }.toMutableList()
+        val value = sortedBy.first().value
 
+        // add J's to the strongest cards
+        sortedBy[0].setValue(value + jInHand)
 
+        return when (sortedBy[0].value) {
+            5 -> 7                                      // five of a kind
+            4 -> 6                                      // 5 of a kind
+            3 -> if (sortedBy[1].value == 2) 5 else 4   // full house else three of a kind
+            2 -> if (sortedBy[1].value == 2) 3 else 2   // two pair else pair
+            else -> 1                                   // high card
+        }
+    }
+
+    val labels = listOf('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J').reversed()
+    return solve(input, ::getValHand, labels)
+}
+
+fun solve(input: List<String>, getValHand: KFunction1<Map<Char, Int>, Int>, labels: List<Char>): Int {
+    // map each line to value for hand, actual hand (needed when comparing 2 hands with the same value) and bid
+    val maps = input.map { line ->
+        val split = line.split(" ")
+        val handValue = getValHand(split[0].groupingBy { it }.eachCount())
+        val bid = split[1].toInt()
+        Triple(handValue, split[0], bid)
+    }
+    val sortedWith = maps.sortedWith(
+        compareBy({ triple -> triple.first },
+            { triple -> labels.indexOf(triple.second[0]) },
+            { triple -> labels.indexOf(triple.second[1]) },
+            { triple -> labels.indexOf(triple.second[2]) },
+            { triple -> labels.indexOf(triple.second[3]) },
+            { triple -> labels.indexOf(triple.second[4]) })
+    )
+    return sortedWith.withIndex().sumOf { triple ->
+        triple.value.third * (triple.index + 1)
+    }
+}
