d527234df712468c39fcc461c517246ca647e7f2
szwester <allenszwester@gmail.com>
2023-12-09 07:03:11 +0100

    day 9 2023 - ref

--- src/main/kotlin/solutions/aoc2023/day09/Solution.kt
+++ src/main/kotlin/solutions/aoc2023/day09/Solution.kt
@@ -5,89 +5,48 @@ import utils.getInts
 
 fun main() {
 
-    val inputLine =
-        Resources.getLines(2023, 9)
-//        Resources.getLinesExample(2023, 9)
+    val inputLine = Resources.getLines(2023, 9)
     println("part1 = ${part1(inputLine)}")
     println("part2 = ${part2(inputLine)}")
 }
 
 
-fun nextElemPart2(list: List<Int>): Int {
-    var listLoc = mutableListOf<MutableList<Int>>()
-    listLoc.add(list.toMutableList())
-    while (!listLoc.last().all { x -> x == 0 }) { // while not all 0
-        val lastVals = listLoc.last()
+fun part1(input: List<String>): Int {
 
-        listLoc.add(mutableListOf())
-        for (i in 0..<lastVals.size - 1) { // count new diffs
-            listLoc.last().add(lastVals[i + 1] - lastVals[i])
-        }
-    }
+    val flatMap = input.map { it.split(" ").flatMap { it.getInts() } }
 
-    var diff = 0
-    for (idx in listLoc.lastIndex downTo 0) {
-        diff = listLoc[idx].first() - diff
-        diff.log("ne diff")
+    return flatMap.sumOf {
+        val diffs = getListOfDiffs(it)
+        diffs.foldRight(0) { y, x -> x + y.last() } as Int
     }
-    return diff
 }
 
 fun part2(input: List<String>): Int {
 
-    input.forEach { it.log("p1") }
-
     val flatMap = input.map { it.split(" ").flatMap { it.getInts() } }
 
-    val sum = flatMap.sumOf {
-        nextElemPart2(it)
+    return flatMap.sumOf {
+        val diffs = getListOfDiffs(it)
+        diffs.foldRight(0) { y, x -> y.first() - x } as Int
     }
-    sum.log("answ")
-
-    return sum
 }
 
-
-fun part1(input: List<String>): Int {
-
-    input.forEach { it.log("p1") }
-
-    val flatMap = input.map { it.split(" ").flatMap { it.getInts() } }
-
-    var sum = flatMap.sumOf {
-        val nextElem = nextElem(it)
-        nextElem.log("nexxt")
-        nextElem
-    }
-
-    sum.log("answ")
-
-    return sum
-}
-
-fun nextElem(list: List<Int>): Int {
-    var listLoc = mutableListOf<MutableList<Int>>()
-    listLoc.add(list.toMutableList())
-    while (!listLoc.last().all { x -> x == 0 }) { // while not all 0
-        val lastVals = listLoc.last()
-
-        listLoc.add(mutableListOf())
+fun getListOfDiffs(list: List<Int>): MutableList<MutableList<Int>> {
+    val diffList = mutableListOf<MutableList<Int>>()
+    diffList.add(list.toMutableList()) // initially add all original elements
+    while (!diffList.last().all { x -> x == 0 }) { // while not all 0
+        val lastVals = diffList.last()
+        diffList.add(mutableListOf())
         for (i in 0..<lastVals.size - 1) { // count new diffs
-            listLoc.last().add(lastVals[i + 1] - lastVals[i])
+            diffList.last().add(lastVals[i + 1] - lastVals[i])
         }
     }
-
-    var diff = 0
-    var value = 0
-    for (idx in listLoc.lastIndex downTo 0) {
-        diff = listLoc[idx].last() + diff
-        diff.log("ne diff")
-    }
-    return diff
+    return diffList
 }
 
-private fun <T> T.log(): T = also { println("%s".format(this)) }
-private fun <T> T.log(comment: String): T = also { println("%s: %s".format(comment, this)) }
+
+
+
 
 
 
