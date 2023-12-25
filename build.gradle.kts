cf0051260c660c3b93338decd65c688a400a8c8e
mo <allenszwester@gmail.com>
2023-12-25 08:54:44 +0100

    Day 25 2023

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ dependencies {
     implementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation(kotlin("test"))
     implementation(kotlin("script-runtime"))
+    // https://mvnrepository.com/artifact/org.jgrapht/jgrapht-core
+    implementation("org.jgrapht:jgrapht-core:1.5.2")
+
 }
 
 tasks.test {
---
+++ src/main/kotlin/solutions/aoc2023/day25/Solution.kt
@@ -0,0 +1,42 @@
+package solutions.aoc2023.day25
+
+import org.jgrapht.Graph
+import org.jgrapht.alg.flow.GusfieldGomoryHuCutTree
+import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.graph.SimpleGraph
+import utils.Resources
+
+
+fun main() {
+
+    val inputLine = Resources.getLines(2023, 25)
+//        Resources.getLinesExample(2023, 25)
+
+    println("part1 = ${part1(inputLine)}")
+
+}
+
+fun part1(input: List<String>): Int {
+
+
+    val graph: Graph<String, DefaultEdge> = SimpleGraph(DefaultEdge::class.java)
+
+    input.forEach {
+
+        val split = it.split(": ")
+        val left = split[0]
+        val rightSplit = split[1].split(" ").toMutableList()
+
+        graph.addVertex(left)
+        rightSplit.forEach {
+            graph.addVertex(it)
+            graph.addEdge(left, it)
+        }
+    }
+
+    val gusfieldGomoryHuCutTree = GusfieldGomoryHuCutTree(graph)
+    gusfieldGomoryHuCutTree.calculateMinCut()
+    val sourcePartition = gusfieldGomoryHuCutTree.sourcePartition
+    val oneSide = graph.vertexSet().size - sourcePartition.size
+    return oneSide * sourcePartition.size
+}
