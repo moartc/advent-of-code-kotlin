fee3eac7885ab8a7f0ec8fffefd6dce4d5adb6f5
mo <allenszwester@gmail.com>
2023-12-26 00:26:29 +0100

    Day 24 2023 part2

--- src/main/kotlin/solutions/aoc2023/day24/Solution.kt
+++ src/main/kotlin/solutions/aoc2023/day24/Solution.kt
@@ -3,7 +3,6 @@ package solutions.aoc2023.day24
 import utils.Resources
 import utils.parser.getLongs
 import java.math.BigInteger
-import kotlin.math.*
 
 fun main() {
 
@@ -11,139 +10,138 @@ fun main() {
         Resources.getLines(2023, 24)
 //        Resources.getLinesExample(2023, 24)
 
-    println("part1 = ${part1(inputLine)}")
+//    println("part1 = ${part1(inputLine)}")
+    println("part2 = ${part2(inputLine)}")
 
 
 }
+
 // 12018 too low
 var range = 200000000000000L..400000000000000L
 //    var range = 7L..27L
 
 data class Hails(var px: Long, var py: Long, var pz: Long, var vx: Long, var vy: Long, var vz: Long) {
 
-    fun movep1() {
-        this.px += vx
-        this.py += vy
-//        this.pz += pz
-    }
-
-//    fun distanceToRange(range: LongRange): Pair<Long, Long> {
-//        var yd = -1
-//        var xd = -1
-//        if (py < range.first) {
-//            yd = range.first - py
-//        } else {
-//            yd = yd - range.last
-//        }
-//        if (py < range.first) {
-//            xd = range.first - px
-//        } else if (py in range && px in range) {
-//            return 0 to 0
-//        }
-//
-//        return yd to xd
-//    }
-
-    fun inAreaWithPast(r: LongRange): LongRange {
-        println("in area for $px $py")
-
-        var yInRangeAfter = -1L
-        var xInRangeAfter = -1L
-
-        var xsir = -1L
-        var ysir = -1L
-
-        var yOnPos = -1L
-        var xOnPos = -1L
-
-
-        if (py < r.first) {
-            if (vy > 0) {
-                var dist = r.first - py
-                yInRangeAfter = ceil(dist / vy.toDouble()).toLong().absoluteValue
-                yOnPos = py + yInRangeAfter * vy
-
-            } else {
-                return -1L..-1L
-            }
-        } else if (py > r.last) {
-            if (vy < 0) {
-                var dist = py - r.last
-                yInRangeAfter = floor(dist / vy.toDouble()).toLong().absoluteValue
-                yOnPos = py - yInRangeAfter * vy
-            } else {
-                return -1L..-1L
-            }
-        }
-
-        if (px < r.first) {
-            if (vx > 0) {
-                var dist = r.first - px
-                xInRangeAfter = ceil(dist / vx.toDouble()).toLong().absoluteValue
-                yOnPos = px + yInRangeAfter * vy
-
-            } else {
-                return -1L..-1L
-            }
-        } else if (px > r.last) {
-            if (vx < 0) {
-                var dist = px - r.last
-                xInRangeAfter = floor(dist / vx.toDouble()).toLong().absoluteValue
-                xOnPos = px - yInRangeAfter * vx
-            }
-        }
-
-        if (py in r) {
-            if (vy > 0) {
-                ysir = (r.last - py) / vy.absoluteValue
-            } else {
-                ysir = (py - r.first) / vy.absoluteValue
-            }
-        }
-
-        if (px in r) {
-            if (vx > 0) {
-                xsir = (r.last - px) / vx.absoluteValue
-            } else {
-                xsir = (px - r.first) / vx.absoluteValue
-            }
-        }
-
-        if (ysir == -1L && yInRangeAfter != -1L) {
-            if (vy > 0) {
-                ysir = (r.last - yOnPos) / vy.absoluteValue
-            } else {
-                ysir = (yOnPos - r.first) / vy.absoluteValue
-            }
-        }
-
-        if (xsir == -1L && xInRangeAfter != -1L) {
-            if (vx > 0) {
-                xsir = (r.last - xOnPos) / vx.absoluteValue
-            } else {
-                xsir = (xOnPos - r.first) / vx.absoluteValue
-            }
-        }
-
-        var yFinS = if (yInRangeAfter == -1L) 0 else yInRangeAfter
-        var xFinS = if (xInRangeAfter == -1L) 0 else xInRangeAfter
-
-        var yFinEnd = yFinS + ysir
-        var xFinEnd = xFinS + xsir
-
-        val yRange = yFinS..yFinEnd
-        val xRange = xFinS..xFinEnd
-
-        yRange.log("yRange")
-        xRange.log("xRange")
-        val intsc = max(yRange.first, xRange.first)..min(yRange.last, xRange.last)
-        intsc.log("intersection")
-        println("------------")
-        return intsc
-    }
-
-
 }
+
+// TODO clean up / rewrite to be able to get the result for the input data without intermediate manual calculations
+fun part2(input: List<String>): Int {
+
+    val allH = mutableListOf<Hails>()
+    input.forEach {
+        val ints = it.getLongs()
+        allH.add(Hails(ints[0], ints[1], ints[2], ints[3], ints[4], ints[5]))
+    }
+
+
+    fun getIntersection(
+        p11: Pair<BigInteger, BigInteger>,    // first point line 1
+        p12: Pair<BigInteger, BigInteger>,// 2nd point line 1
+        p21: Pair<BigInteger, BigInteger>,// first point line 2
+        p22: Pair<BigInteger, BigInteger> // 2nd point line 2
+    ): Pair<BigInteger, BigInteger> {
+        // Line AB represented as a1x + b1y = c1
+        val a1 = p12.first - p11.first;
+        val b1 = p11.second - p12.second;
+        val c1 = a1 * (p11.second) + b1 * (p11.first);
+
+        // Line CD represented as a2x + b2y = c2
+        val a2 = p22.first - p21.first;
+        val b2 = p21.second - p22.second;
+        val c2 = a2 * (p21.second) + b2 * (p21.first);
+
+        val determinant = a1 * b2 - a2 * b1;
+
+        if (determinant == BigInteger.ZERO) {
+            return BigInteger.ZERO to BigInteger.ZERO
+        } else {
+            val y = (b2 * c1 - b1 * c2).divide(determinant)
+            val x = (a1 * c2 - a2 * c1).divide(determinant)
+            return x to y
+        }
+    }
+
+
+    // select 3 random
+    var first = allH[1]
+    var second = allH[2]
+    var third = allH[3]
+
+    // random velocities to test
+    for (nvx in -700..700) {
+        for (nvy in -700..700) {
+
+            // instead of moving stone, move hailstones with changed velocity
+            val firstChanged = first.copy(vx = first.vx - nvx, vy = first.vy - nvy)
+            val secondChanged = second.copy(vx = second.vx - nvx, vy = second.vy - nvy)
+            val thirdChanged = third.copy(vx = third.vx - nvx, vy = third.vy - nvy)
+
+            val fs = firstChanged.px.toBigInteger() to firstChanged.py.toBigInteger()
+            val fe =
+                firstChanged.px.toBigInteger() + firstChanged.vx.toBigInteger() to (firstChanged.py.toBigInteger() + firstChanged.vy.toBigInteger())
+
+            val ss = secondChanged.px.toBigInteger() to secondChanged.py.toBigInteger()
+            val se =
+                secondChanged.px.toBigInteger() + secondChanged.vx.toBigInteger() to (secondChanged.py + secondChanged.vy).toBigInteger()
+
+            val intersection1 = getIntersection(fs, fe, ss, se)
+
+
+            val ts = thirdChanged.px.toBigInteger() to thirdChanged.py.toBigInteger()
+            val te =
+                thirdChanged.px.toBigInteger() + thirdChanged.vx.toBigInteger() to (thirdChanged.py + thirdChanged.vy).toBigInteger()
+
+            val intersection2 = getIntersection(fs, fe, ts, te)
+            if ((intersection2.first - intersection1.first).abs() < BigInteger.TWO && (intersection2.second - intersection1.second).abs() < BigInteger.TWO) {
+                println("intersection == 0 for nvx=$nvx and nvy=$nvy")
+                println("intersection1 $intersection1")
+                println("intersection2 $intersection2") // it found intersection1 (reversed y x) (441968877324087, 218159652637145) for nvx=131 and nvy=-259
+            }
+        }
+    }
+
+    // change it after result from line aboce
+    val newStart = Hails(218159652637145, 441968877324087, 0, 131, -259, 0)
+//    val newStart = Hails(24, 13, 0, -3, 1, 0) // for test
+    var z: BigInteger = BigInteger.ZERO
+
+    val newS = newStart.px.toBigInteger() to newStart.py.toBigInteger()
+    val newE = (newStart.px + newStart.vx).toBigInteger() to (newStart.py + newStart.vy).toBigInteger()
+    val fS = first.px.toBigInteger() to first.py.toBigInteger()
+    val fE = first.px.toBigInteger() + first.vx.toBigInteger() to (first.py + first.vy).toBigInteger()
+    val sS = second.px.toBigInteger() to second.py.toBigInteger()
+    val sE = second.px.toBigInteger() + second.vx.toBigInteger() to (second.py + second.vy).toBigInteger()
+    val intersectionWithFirst = getIntersection(newS, newE, fS, fE)
+    val intersectionWithSecond = getIntersection(newS, newE, sS, sE)
+
+    intersectionWithFirst.log("int with first")
+    val steps = (intersectionWithFirst.first - first.px.toBigInteger()).divide(first.vx.toBigInteger())
+    println("after $steps steps z is on pos ${first.pz.toBigInteger() + (steps * first.vz.toBigInteger())}")
+    intersectionWithSecond.log("int with sec")
+    val steps2 = (intersectionWithSecond.first - second.px.toBigInteger()).divide(second.vx.toBigInteger())
+    println("after $steps2 steps z is on pos ${second.pz.toBigInteger() + (steps2 * second.vz.toBigInteger())}")
+
+    /*
+    after 133276408329 steps z is on pos 234012912244761
+    after 504617559466 steps z is on pos 271889709660735
+    271889709660735 = pz + vz * 504617559466
+    234012912244761 = pz + vz * 133276408329
+    =
+    37876797415974 = vz 371341151137 => vz = 102
+    234012912244761 = pz + 102 * 133276408329
+    234012912244761 = pz + 13594193649558
+    pz = 220418718595203
+
+
+    final res (x, y, z) = (218159652637145,441968877324087,220418718595203)
+    sum = 880547248556435
+     */
+//
+
+
+    return 12
+}
+
 
 fun part1(input: List<String>): Int {
 
@@ -156,31 +154,21 @@ fun part1(input: List<String>): Int {
         allH.add(Hails(ints[0], ints[1], ints[2], ints[3], ints[4], ints[5]))
     }
 
-
-
-
-    val associateWith = allH.associateWith {
-        it.inAreaWithPast(range)
-    }
-
-    // ----------------
-
-
     fun intersectionOf2LinesGoingThroughPoints(
-        A: Pair<BigInteger, BigInteger>,    // first point line 1
-        B: Pair<BigInteger, BigInteger>,// 2nd point line 1
-        C: Pair<BigInteger, BigInteger>,// first point line 2
-        D: Pair<BigInteger, BigInteger> // 2nd point line 2
+        p11: Pair<BigInteger, BigInteger>,    // first point line 1
+        p12: Pair<BigInteger, BigInteger>,// 2nd point line 1
+        p21: Pair<BigInteger, BigInteger>,// first point line 2
+        p22: Pair<BigInteger, BigInteger> // 2nd point line 2
     ): Pair<BigInteger, BigInteger> {
         // Line AB represented as a1x + b1y = c1
-        val a1 = B.first - A.first;
-        val b1 = A.second - B.second;
-        val c1 = a1 * (A.second) + b1 * (A.first);
+        val a1 = p12.first - p11.first;
+        val b1 = p11.second - p12.second;
+        val c1 = a1 * (p11.second) + b1 * (p11.first);
 
         // Line CD represented as a2x + b2y = c2
-        val a2 = D.first - C.first;
-        val b2 = C.second - D.second;
-        val c2 = a2 * (C.second) + b2 * (C.first);
+        val a2 = p22.first - p21.first;
+        val b2 = p21.second - p22.second;
+        val c2 = a2 * (p21.second) + b2 * (p21.first);
 
         val determinant = a1 * b2 - a2 * b1;
 
@@ -198,51 +186,6 @@ fun part1(input: List<String>): Int {
     val doubleRange = range.first.toDouble()..range.last.toDouble()
 
     var ctr = 0
-//    for ((index1, h1) in allH.withIndex()) {
-//        println("for h1 = $h1")
-//        val r1 = associateWith[h1]!!
-//        val fxs = h1.px + (h1.vx * r1.first)
-//        val fxe = h1.px + (h1.vx * r1.last)
-//        val fys = h1.py + (h1.vy * r1.first)
-//        val fye = h1.py + (h1.vy * r1.last)
-//
-//        val a = fxs to fys
-//        val b = fxe to fye
-//        println("1st start $a end $b")
-//        for ((index2, h2) in allH.withIndex()) {
-//            val r2 = associateWith[h2]!!
-//            if (index1 < index2) {
-//                val sxs = h2.px + (h2.vx * r2.first)
-//                val sxe = h2.px + (h2.vx * r2.last)
-//                val sys = h2.py + (h2.vy * r2.first)
-//                val sye = h2.py + (h2.vy * r2.last)
-//
-//                val c = sxs to sys
-//                val d = sxe to sye
-//
-//                println("for h2 = $h2")
-//                println("2nd start $c end $d")
-//
-//                val lineLineIntersection = intersectionOf2LinesGoingThroughPoints(a, b, c, d)
-//                println("!!!!!!!intersection between $h1 and $h2 is = $lineLineIntersection")
-//
-//                if (lineLineIntersection.first in doubleRange && lineLineIntersection.second in doubleRange) {
-//                    // additional check if intersection is in the current range
-//                    val intX = lineLineIntersection.second
-//                    val intY = lineLineIntersection.first
-//
-//                    val firstXOk = intX in min(fxs.toDouble(), fxe.toDouble())..max(fxs.toDouble(), fxe.toDouble())
-//                    val firstYOk = intY in min(fys.toDouble(), fye.toDouble())..max(fys.toDouble(), fye.toDouble())
-//                    val secXOk = intX in min(sxs.toDouble(), sxe.toDouble())..max(sxs.toDouble(), sxe.toDouble())
-//                    val secYOk = intY in min(sys.toDouble(), sye.toDouble())..max(sys.toDouble(), sye.toDouble())
-//                    if (firstYOk && firstXOk && secXOk && secYOk) {
-//                        lineLineIntersection.log(">>>>>>>>>>>>ctr++ for line intersection")
-//                        ctr++
-//                    }
-//                }
-//            }
-//        }
-//    }
     for ((index1, h1) in allH.withIndex()) {
         println("for h1 = $h1")
         val fxs = h1.px
@@ -267,7 +210,7 @@ fun part1(input: List<String>): Int {
                 println("2nd start $c end $d")
 
                 val lineLineIntersection = intersectionOf2LinesGoingThroughPoints(a, b, c, d)
-                if(lineLineIntersection.first != BigInteger.ZERO && lineLineIntersection.second != BigInteger.ZERO) {
+                if (lineLineIntersection.first != BigInteger.ZERO && lineLineIntersection.second != BigInteger.ZERO) {
                     println("!!!!!!!intersection between $h1 and $h2 is = $lineLineIntersection")
 
                     if (lineLineIntersection.first.toDouble() in doubleRange && lineLineIntersection.second.toDouble() in doubleRange) {
@@ -275,11 +218,11 @@ fun part1(input: List<String>): Int {
                         val intX = lineLineIntersection.second.toDouble()
                         val intY = lineLineIntersection.first.toDouble()
 
-                        val firstXOk = (intX >= fxs && h1.vx >=0) || (intX <= fxs && h1.vx <0)
-                        val firstYOk = (intY >= fys && h1.vy >=0) || (intY <= fys && h1.vy <0)
+                        val firstXOk = (intX >= fxs && h1.vx >= 0) || (intX <= fxs && h1.vx < 0)
+                        val firstYOk = (intY >= fys && h1.vy >= 0) || (intY <= fys && h1.vy < 0)
 
-                        val secXOk = (intX >= sxs && h2.vx >=0) || (intX <= sxs && h2.vx <0)
-                        val secYOk = (intY >= sys && h2.vy >=0) || (intY <= sys && h2.vy <0)
+                        val secXOk = (intX >= sxs && h2.vx >= 0) || (intX <= sxs && h2.vx < 0)
+                        val secYOk = (intY >= sys && h2.vy >= 0) || (intY <= sys && h2.vy < 0)
                         if (firstYOk && firstXOk && secXOk && secYOk) {
                             lineLineIntersection.log(">>>>>>>>>>>>ctr++ for line intersection")
                             ctr++
@@ -290,22 +233,7 @@ fun part1(input: List<String>): Int {
             }
         }
     }
-
-//    val lineLineIntersection = lineLineIntersection(a, b, c, d)
-//    lineLineIntersection.log("line intersection test")
-
-
     return ctr
-}
-
-fun crossInside(h1: Hails, h2: Hails, s: Int, e: Int): Boolean {
-    val range = s..e
-    if (h1.px in range && h2.px in range && h1.py in range && h2.py in range) {
-        return true
-    } else {
-        return false
-    }
-
 }
 
 
