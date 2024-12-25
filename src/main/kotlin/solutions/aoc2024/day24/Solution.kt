958972073e0d43d27a118d43d60bca49f4db6bc4
szwester <allenszwester@gmail.com>
2024-12-25 22:52:18 +0100

    Day 24 - refactoring

--- src/main/kotlin/solutions/aoc2024/day24/Solution.kt
+++ src/main/kotlin/solutions/aoc2024/day24/Solution.kt
@@ -2,7 +2,6 @@ package solutions.aoc2024.day24
 
 import utils.Resources
 import utils.collections.extensions.splitOnEmpty
-import java.math.BigInteger
 
 val day = (object {}).javaClass.packageName.takeLast(2).toInt()
 
@@ -14,8 +13,44 @@ fun main() {
     println("part2 = ${part2(inputLines)}")
 }
 
-// todo clean it up and automate somehow
-fun part2(inputLines: List<String>): Any {
+fun part1(inputLines: List<String>): Long {
+    val splitOnEmpty = inputLines.splitOnEmpty()
+    val firstPart = splitOnEmpty[0]
+    val secondPart = splitOnEmpty[1]
+    val initialValues = firstPart.map {
+        val s = it.split(": ")
+        s[0] to s[1].toInt()
+    }.toMap()
+
+    val gates = secondPart.map {
+        val v = it.split(" -> ")
+        val p1 = v[0]
+        val p2 = v[1]
+        p1 to p2
+    }
+    val binaryResult = simulate(initialValues, gates)
+    return binaryResult.toLong(2)
+}
+
+/*
+automated - at least for my input
+only 2 cases are supported:
+1. the expression assigned to the output is invalid, e.g:
+psw OR nng -> z12
+but, it should be:
+nhb XOR cdq -> z12
+expected value (nhb XOR cdq) is assign to 'kth':
+nhb XOR cdq -> kth
+so replace 'z12' with 'kth'
+
+2. the value in the expression is incorrect
+expected
+htb AND vpm
+but, it contains:
+htb AND qnf
+because there is no whole expression 'htb AND vpm' that could be used - 'qnf' value is replaced by 'vpm' value.
+ */
+fun part2(inputLines: List<String>): String {
     val splitOnEmpty = inputLines.splitOnEmpty()
     val firstPart = splitOnEmpty[0]
     val secondPart = splitOnEmpty[1]
@@ -33,202 +68,127 @@ fun part2(inputLines: List<String>): Any {
     }
 
     val midIndex = initialValues.values.size / 2
-    val firstHalf = initialValues.values.toList().subList(0, midIndex).reversed()
-    val secondHalf = initialValues.values.toList().subList(midIndex, initialValues.values.size).toList().reversed()
+    val xBitsList = initialValues.values.take(midIndex).reversed()
+    val yBitsList = initialValues.values.drop(midIndex).reversed()
+    val xBinaryString = xBitsList.joinToString("")
+    val yBinaryString = yBitsList.joinToString("")
+    val xLong = xBinaryString.toLong(2)
+    val yLong = yBinaryString.toLong(2)
 
-    firstHalf.log("first")
-    secondHalf.log("second")
+    val expectedSum = xLong + yLong
+    val expectedBinaryArr = expectedSum.toString(2).map { it.digitToInt() }
 
-    val bin1 = firstHalf.joinToString("")
-    val bin2 = secondHalf.joinToString("")
-
-    bin1.log("bin1")
-    bin2.log("bin2")
-
-    val bigInteger1 = BigInteger(bin1, 2)
-    val bigInteger2 = BigInteger(bin2, 2)
-
-    bigInteger1.log("big int 1")
-    bigInteger2.log("big int 2")
-
-    val expectedSum = bigInteger1 + bigInteger2
-    expectedSum.log("exp")
-
-    val expectedBinary = expectedSum.toString(2)
-    expectedBinary.log("expected binary")
-    val expectedIntArr = expectedBinary.map { it.digitToInt() }.toIntArray()
-
-    val binaryResult = calcRes(initialValues, gates)
-    val actualArray = binaryResult.map { it.digitToInt() }
+    fun isCurrentResultAsExpected(currentArray: List<Int>, expectedArray: List<Int>): Boolean {
+        for (index in currentArray.indices) {
+            if (currentArray[index] != expectedArray[index]) {
+                return false
+            }
+        }
+        return true
+    }
 
     /*
-     for  z02
-     z02 = rsk XOR rhr
-     rsk = x02 XOR y02
-     rhr = cbq OR gwd
-     gwd = y01 AND x01
-     cbq = twd AND bdj
-     twd = y01 XOR x01
-     bdj = x00 AND y00
-
-     for z03
-     z03 = jfr XOR twj
-     jfr = x03 XOR y03
-     twj = fph OR nkm
-     nkm = rhr AND rsk - looks like z02 but with AND
-     fph = y02 AND x02
-
-     for z04
-     z04 = dft XOR vwf  - so it's always xor
-     dft = nbj OR rnw
-     vwf = y04 XOR x04  - it's always yn XOR xn for zn
-     nbj = twj AND jfr  - again - like z03 but with and
-     rnw = y03 AND x03
-     ... and probably so on
+     for z04 - n=04
+     z04 = A XOR B  - so it's always xor
+     A = C OR D
+     B = y04 XOR x04  - it's yn XOR xn
+     C = E AND F  - it's z{n-1} but with AND
+     D = y03 AND x03  - it's x{n-1} AND y{n-1}
      */
-
-    fun isOk(zToCheck: String, gates: List<Pair<String, String>>) {
-        var numb = zToCheck.takeLast(2)
-        val gateMap = mutableMapOf<String, String>()
+    fun check(zToCheck: String, gates: List<Pair<String, String>>): Pair<String, String>? {
+        val zIndex = zToCheck.takeLast(2)
+        val prevNumb = zIndex.toInt() - 1
+        val prevNumStr = if (prevNumb < 10) "0$prevNumb" else prevNumb
+        val gatesMap = mutableMapOf<String, String>()
         gates.forEach { gate ->
-            gateMap[gate.second] = gate.first
+            gatesMap[gate.second] = gate.first
         }
-        val first = gateMap[zToCheck]
+        // expected values based on patter
+        val prevZId = "z$prevNumStr"
+        val prevZ = gatesMap[prevZId]!!
+        val (expectedValueE, _, expectedValueF) = prevZ.split(" ")
+        val expectedValueB = listOf("x$zIndex", "XOR", "y$zIndex")
+        val expectedValueC = setOf(expectedValueE, "AND", expectedValueF) //twj and jfr
+        val expectedValueD = setOf("x$prevNumStr", "AND", "y$prevNumStr")
 
-        val (left, op, right) = first!!.split(" ")
-        if (op != "XOR") {
-            println("operator should be xor!!!")
-            return
-        }
-        val lToCheck1 = gateMap[left]!!
-        val rToCheck1 = gateMap[right]!!
-        val (ll, lo, lr) = lToCheck1.split(" ")
-        val (rl, ro, rr) = rToCheck1.split(" ")
-        // one of them should be xn yn with or
-        if (lo != "XOR" && ro != "XOR") {
-            println("missing xor!")
-        } else if (lo == "XOR") {
-            if (!setOf(ll, lr).sorted().containsAll(setOf("x$numb", "y$numb"))) {
-                println("no x y atts")
-            }
-        } else {
-            // here should be xor for ro
-            if (!setOf(rl, rr).sorted().containsAll(setOf("x$numb", "y$numb"))) {
-                println("no x y atts")
-            }
-        }
-        var wasLeftXor = lo == "XOR"
+        val actualBVariableName = gatesMap.filter { p ->
+            p.value.split(" ").toSet().containsAll(expectedValueB)
+        }.map { e -> e.key }.first()
 
-        var (ltodo, optodo, rtodo) = if (wasLeftXor) rToCheck1.split(" ") else lToCheck1.split(" ")
+        val actualCVariableName = gatesMap.filter { p ->
+            p.value.split(" ").toSet().containsAll(expectedValueC)
+        }.map { e -> e.key }.first()
 
-        if (optodo != "OR") {
-            println("op should be or!!!")
-        }
+        val actualDVariableName = gatesMap.filter { p ->
+            p.value.split(" ").toSet().containsAll(expectedValueD)
+        }.map { e -> e.key }.first()
 
-        var prevNumb = numb.toInt() - 1
-        var prevNumStr = if (prevNumb < 10) "0$prevNumb" else prevNumb
-        var prevZ = gateMap["z$prevNumStr"]!!
+        val actualAVariableName = gatesMap.filter { p ->
+            p.value.split(" ").toSet().containsAll(setOf(actualCVariableName, "OR", actualDVariableName))
+        }.map { e -> e.key }.first()
 
+        val valuesThatShouldContainZ = setOf(actualAVariableName, "XOR", actualBVariableName)
+        val valueThatShouldBeAssignToZ = gatesMap.filter { p ->
+            p.value.split(" ").toSet().containsAll(valuesThatShouldContainZ)
+        }.map { e -> e.key }.firstOrNull()
 
-        val left2 = gateMap[ltodo]!!
-        val right2 = gateMap[rtodo]!!
-
-        var (l3, o3, r3) = left2.split(" ")
-        var (l4, o4, r4) = right2.split(" ")
-
-        if (!l3.startsWith("x")
-            && !r3.startsWith("x")
-            && !l4.startsWith("x")
-            && !r4.startsWith("x")
-        ) {
-            println("missing x here <-!")
-        }
-
-        var foundInFirst = false
-        if (l3.startsWith("x") || r3.startsWith("x")) {
-            foundInFirst = true
-            if (!setOf(l3, r3).containsAll(setOf("x${prevNumStr}", "y${prevNumStr}"))) {
-                println("some problem 123")
-            }
-            if (o3 != "AND") {
-                println("No AND op!")
-            }
-        } else {
-            if (!setOf(l4, r4).containsAll(setOf("x${prevNumStr}", "y${prevNumStr}"))) {
-                println("some problem 123")
-            }
-            if (o4 != "AND") {
-                println("No AND op!")
+        // check actual values
+        val actualZ = gatesMap[zToCheck]!!
+        val actualZContent = actualZ.split(" ").toSet()
+        if (!actualZContent.containsAll(valuesThatShouldContainZ)) {
+            println("z doesn't contain: $valuesThatShouldContainZ, but contains $actualZContent")
+            if (valueThatShouldBeAssignToZ != null) {
+                println("the content of z is = $valueThatShouldBeAssignToZ")
+                println("to swap '$zToCheck' with '$valueThatShouldBeAssignToZ'")
+                return zToCheck to valueThatShouldBeAssignToZ
+            } else {
+                val missingValue = valuesThatShouldContainZ.minus(actualZContent).first()
+                val unwantedValue = actualZContent.minus(valuesThatShouldContainZ).first()
+                println("to swap '$missingValue' with '$unwantedValue'")
+                return missingValue to unwantedValue
             }
         }
-        // x is check now if it's line in prev z
-        val (prevZL, _, prevZR) = prevZ.split(" ")
-
-        val (left5, op5, right5) = if (foundInFirst) right2.split(" ") else left2.split(" ")
-
-        if (!setOf(prevZL, prevZR).containsAll(setOf(left5, right5))) {
-            println(
-                "here is missing somethinf!\n" +
-                        "${setOf(prevZL, prevZR)} should contain${setOf(left5, right5)}"
-            )
-        }
-        if (op5 != "AND") {
-            println("op is not And!")
-        }
+        return null
     }
 
-    /*
-    Changes manual
-    vpm -> qnf because of missing xor
-    z32 -> tbt
-    kth -> z12
-    z26 -> gsd
-     */
-
-    // answer part, how many wrong should be 0 below!
-    setOf("vpm", "qnf", "z32", "tbt", "kth", "z12", "z26", "gsd").sorted().joinToString(",").log("answer")
-
-    // -------------------------
-    val toCheck = gates.map { it.second }.filter { it.startsWith("z") && it != "z00" && it != "z01" }.sorted()
-    toCheck.forEach { g ->
-        println("-------------- checking $g ---------------")
-        isOk(g, gates)
-        println("-----------------------------------------")
+    fun swapGates(first: String, second: String, gates: MutableList<Pair<String, String>>): MutableList<Pair<String, String>> {
+        val indexOfFirst = gates.indexOfFirst { g -> g.second == first }
+        val indexOfSecond = gates.indexOfFirst { g -> g.second == second }
+        gates[indexOfFirst] = gates[indexOfFirst].first to second
+        gates[indexOfSecond] = gates[indexOfSecond].first to first
+        return gates
     }
 
-
-    var howManyWrong = 0
-    for (index in actualArray.indices) {
-        if (actualArray[index] != expectedIntArr[index]) {
-            howManyWrong++
+    // skip first 2 and check for the rest sorted alphabetically z02, z03, z04,...
+    val swappedOutputs = mutableListOf<String>()
+    var gatesToUpdate = gates.toMutableList()
+    wh@ while (true) {
+        // skip first 2 and the last one
+        val toCheck = gatesToUpdate.map { it.second }.filter { it.startsWith("z") }.sorted().drop(2).dropLast(1)
+        for (g in toCheck) {
+            val checkResult = check(g, gatesToUpdate)
+            if (checkResult != null) {
+                val (first, second) = checkResult
+                gatesToUpdate = swapGates(first, second, gatesToUpdate)
+                swappedOutputs.add(first)
+                swappedOutputs.add(second)
+                continue@wh
+            }
         }
+        break
     }
-    howManyWrong.log("there was wrong:")
-    println(binaryResult)
-    return 12
+
+    // check if it's correct
+    val simulationResultAfterFixes = simulate(initialValues, gatesToUpdate).map { it.digitToInt() }
+
+    if (!isCurrentResultAsExpected(simulationResultAfterFixes, expectedBinaryArr)) {
+        throw Exception("The result is not correct after fixes!")
+    }
+
+    return swappedOutputs.sorted().joinToString(",")
 }
 
-fun part1(inputLines: List<String>): BigInteger {
-    val splitOnEmpty = inputLines.splitOnEmpty()
-    val firstPart = splitOnEmpty[0]
-    val secondPart = splitOnEmpty[1]
-    val initialValues = firstPart.map {
-        val s = it.split(": ")
-        s[0] to s[1].toInt()
-    }.toMap()
-
-    val gates = secondPart.map {
-        val v = it.split(" -> ")
-        val p1 = v[0]
-        val p2 = v[1]
-        p1 to p2
-    }
-
-    val binaryResult = calcRes(initialValues, gates)
-    return BigInteger(binaryResult, 2)
-}
-
-fun calcRes(initialValues: Map<String, Int>, gates: List<Pair<String, String>>): String {
+fun simulate(initialValues: Map<String, Int>, gates: List<Pair<String, String>>): String {
     val wires = initialValues.toMutableMap()
     while (true) {
         var updated = false
@@ -253,7 +213,6 @@ fun calcRes(initialValues: Map<String, Int>, gates: List<Pair<String, String>>):
             wires[output] = result
             updated = true
         }
-
         if (!updated) {
             break
         }
@@ -263,6 +222,3 @@ fun calcRes(initialValues: Map<String, Int>, gates: List<Pair<String, String>>):
     val binaryResult = zValues.entries.sortedByDescending { it.key }.joinToString("") { it.value.toString() }
     return binaryResult
 }
-
-private fun <T> T.log(): T = also { println("%s".format(this)) }
-private fun <T> T.log(comment: String): T = also { println("%s: %s".format(comment, this)) }
