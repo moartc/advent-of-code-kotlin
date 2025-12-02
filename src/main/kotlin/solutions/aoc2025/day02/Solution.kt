8b0b163907d82b5cde2dd12f47b5feadcf7336ba
mo <allenszwester@gmail.com>
2025-12-02 18:31:46 +0100

    Day 2 2025 - cleanup

--- src/main/kotlin/solutions/aoc2025/day02/Solution.kt
+++ src/main/kotlin/solutions/aoc2025/day02/Solution.kt
@@ -8,7 +8,6 @@ val day = (object {}).javaClass.packageName.takeLast(2).toInt()
 fun main() {
 
     val inputLines = Resources.getLines(2025, day)
-
     println("part1 = ${part1(inputLines)}")
     println("part2 = ${part2(inputLines)}")
 }
@@ -22,23 +21,18 @@ fun part1(inputLines: List<String>): BigInteger {
     }
 
     fun shouldCount(s: String): Boolean {
-        if (s.length % 2 != 0) {
-            return false
-        }
         val mid = s.length / 2
         return s.take(mid) == s.substring(mid)
     }
 
     var sum = BigInteger.ZERO
     for ((l, r) in listOfPair) {
-        val start = l.toBigInteger()
-        val end = r.toBigInteger()
-        var i = start
-        while (i <= end) {
+        var i = l
+        while (i <= r) {
             if (shouldCount(i.toString())) {
-                sum += i
+                sum += i.toBigInteger()
             }
-            i += BigInteger.ONE
+            i++
         }
     }
     return sum
@@ -53,10 +47,10 @@ fun part2(inputLines: List<String>): BigInteger {
     }
 
     fun shouldCount(s: String): Boolean {
-        val sSize = s.length
-        ex@ for (i in 2..sSize) {
-            if (sSize % i == 0) {
-                val size = sSize / i
+        val fullSize = s.length
+        ex@ for (i in 2..fullSize) {
+            if (fullSize % i == 0) {
+                val size = fullSize / i
                 val firstPart = s.take(size)
                 for (j in 1 until i) {
                     if (s.substring(j * size, (j + 1) * size) != firstPart) {
@@ -71,20 +65,14 @@ fun part2(inputLines: List<String>): BigInteger {
 
     var sum = BigInteger.ZERO
     for ((l, r) in listOfPair) {
-        val start = l.toBigInteger()
-        val end = r.toBigInteger()
-        var i = start
-        while (i <= end) {
+        var i = l
+        while (i <= r) {
             if (shouldCount(i.toString())) {
-                sum += i
+                sum += i.toBigInteger()
             }
             i++
         }
     }
     return sum
 }
-
-
-private fun <T> T.log(): T = also { println("%s".format(this)) }
-private fun <T> T.log(comment: String): T = also { println("%s: %s".format(comment, this)) }
 
