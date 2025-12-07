d7328a2d6c93ee1a5839f9c8183c20a90800ba28
mo <allenszwester@gmail.com>
2025-12-07 06:49:23 +0100

    Day 7 2025 - clean up

--- src/main/kotlin/solutions/aoc2025/day07/Solution.kt
+++ src/main/kotlin/solutions/aoc2025/day07/Solution.kt
@@ -7,9 +7,7 @@ val day = (object {}).javaClass.packageName.takeLast(2).toInt()
 
 fun main() {
 
-    val inputLines =
-        Resources.getLines(2025, day)
-//    Resources.getLinesExample(2025, day)
+    val inputLines = Resources.getLines(2025, day)
 
     println("part1 = ${part1(inputLines)}")
     println("part2 = ${part2(inputLines)}")
@@ -21,18 +19,16 @@ fun part1(inputLines: List<String>): Long {
     val grid = inputLines.map { x -> x.toCharArray() }
     var beams = mutableSetOf<Pair<Int, Int>>()
     beams.add((y to x))
-    var sc = 0L
+    var ctr = 0L
     while (beams.isNotEmpty()) {
         val ns = mutableSetOf<Pair<Int, Int>>()
         beams.forEach { p ->
-            if (p.first == grid.lastIndex) {
-                // skip
-            } else {
+            if (p.first != grid.lastIndex) {
                 val newPos = p.first + 1 to p.second
                 if (grid[p.first][p.second] == '^') {
                     ns.add(newPos.first to newPos.second - 1)
                     ns.add(newPos.first to newPos.second + 1)
-                    sc++
+                    ctr++
                 } else {
                     ns.add(newPos.first to newPos.second)
                 }
@@ -41,7 +37,7 @@ fun part1(inputLines: List<String>): Long {
         beams = ns
     }
 
-    return sc
+    return ctr
 }
 
 fun part2(inputLines: List<String>): Any {
@@ -50,11 +46,11 @@ fun part2(inputLines: List<String>): Any {
     val grid = inputLines.map { x -> x.toCharArray() }
     var beams = mutableMapOf<Pair<Int, Int>, Long>()
     beams[(y to x)] = 1
-    while (beams.isNotEmpty()) {
+    while (true) {
         val nm = mutableMapOf<Pair<Int, Int>, Long>()
         beams.forEach { p ->
             if (p.key.first == grid.lastIndex) {
-                // all of them should be here
+                // all beams should have the same 'y'
                 return beams.values.sum()
             } else {
                 val newPos = p.key.first + 1 to p.key.second
@@ -69,11 +65,5 @@ fun part2(inputLines: List<String>): Any {
         }
         beams = nm
     }
-    return beams.values.sum()
 }
-// 352321814
-
-
-private fun <T> T.log(): T = also { println("%s".format(this)) }
-private fun <T> T.log(comment: String): T = also { println("%s: %s".format(comment, this)) }
 
